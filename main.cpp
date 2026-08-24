a6a7b4a4176201ea014698b1d21f61b9788a8408
Neeli Satwik <neelisatwik1@gmail.com>
2026-08-24 13:59:58 +0530

    start build-shell

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// TODO (tokenize): implement per the lesson description.
+
+int main() {
+    string line;
+    while (getline(cin, line)) {
+        if (line.empty()) continue;
+        cout << "TODO" << "\n";
+    }
+}
